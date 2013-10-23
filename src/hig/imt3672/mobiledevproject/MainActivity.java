18399900e9038caefaf80fd6819a3b01ce164829
PK <pekaaw@gmail.com>
2013-10-23 17:43:19 +0200

    NOT WORKING: Trying to get a Dialog to work instead of popup.

--- src/hig/imt3672/mobiledevproject/MainActivity.java
+++ src/hig/imt3672/mobiledevproject/MainActivity.java
@@ -2,7 +2,9 @@ package hig.imt3672.mobiledevproject;
 
 import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -13,7 +15,9 @@ import android.widget.Button;
 import android.widget.PopupWindow;
 
 public class MainActivity extends Activity {
-
+	
+	private static final int DIALOG_ADD_ROOM = 0;
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -24,10 +28,33 @@ public class MainActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				View popupView = getLayoutInflater().inflate(R.layout.add_name_popup, null);
-				PopupWindow popup = new PopupWindow(popupView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, true);
-				popup.setAnimationStyle(android.R.style.Animation_Dialog);
-				popup.showAtLocation(popupView, Gravity.CENTER, 0, 0);
+				AlertDialog.Builder builder = new AlertDialog.Builder(getBaseContext());
+				View dialogView = getLayoutInflater().inflate(R.layout.add_name_dialog, null);
+				
+				builder.setView( dialogView )
+					.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							// code to insert new room							
+						}
+					})
+					.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							dialog.cancel();
+						}
+					});
+				
+				AlertDialog dialog = builder.create();
+				dialog.show();
+				
+				
+//				View popupView = getLayoutInflater().inflate(R.layout.add_name_popup, null);
+//				PopupWindow popup = new PopupWindow(popupView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, true);
+//				popup.setAnimationStyle(android.R.style.Animation_Dialog);
+//				popup.showAtLocation(popupView, Gravity.CENTER, 0, 0);
 //				
 //				LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 //				
