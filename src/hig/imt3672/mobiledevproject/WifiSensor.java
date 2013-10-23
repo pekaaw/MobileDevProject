6cf0953b5172158729793e8f4d254489bd44865e
Sveinung <Trresdal>
2013-10-23 13:01:32 +0200

    Moved WifiSensor and now we obtain the network results.

---
+++ src/hig/imt3672/mobiledevproject/WifiSensor.java
@@ -0,0 +1,29 @@
+package hig.imt3672.mobiledevproject;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import android.app.Service;
+import android.content.Intent;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.IBinder;
+
+public class WifiSensor extends Service {
+	WifiManager wifi;
+	List<ScanResult> networks;
+    
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		networks = wifi.getScanResults();
+		Collections.sort(networks,new Comparator<ScanResult>() {
+			@Override
+	        public int compare(ScanResult s1, ScanResult s2) {
+	            return s1.BSSID.compareToIgnoreCase(s2.BSSID);
+	        }
+		});
+		return null;
+	}
+}
