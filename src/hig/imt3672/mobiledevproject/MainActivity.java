bf7116bdb146ebac954f68efa77277f74607a07c
PK <pekaaw@gmail.com>
2013-10-23 18:25:37 +0200

    NOT WORKING: Trying to implement an dialog. Failing.

--- src/hig/imt3672/mobiledevproject/MainActivity.java
+++ src/hig/imt3672/mobiledevproject/MainActivity.java
@@ -1,18 +1,12 @@
 package hig.imt3672.mobiledevproject;
 
-import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
+import android.app.DialogFragment;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.PopupWindow;
+import android.view.ViewGroup;
 
 public class MainActivity extends Activity {
 	
@@ -23,46 +17,46 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		Button button = (Button) findViewById(R.id.add_room_btn);
-		button.setOnClickListener( new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				AlertDialog.Builder builder = new AlertDialog.Builder(getBaseContext());
-				View dialogView = getLayoutInflater().inflate(R.layout.add_name_dialog, null);
-				
-				builder.setView( dialogView )
-					.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-						
-						@Override
-						public void onClick(DialogInterface dialog, int which) {
-							// code to insert new room							
-						}
-					})
-					.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
-						
-						@Override
-						public void onClick(DialogInterface dialog, int which) {
-							dialog.cancel();
-						}
-					});
-				
-				AlertDialog dialog = builder.create();
-				dialog.show();
-				
-				
-//				View popupView = getLayoutInflater().inflate(R.layout.add_name_popup, null);
-//				PopupWindow popup = new PopupWindow(popupView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, true);
-//				popup.setAnimationStyle(android.R.style.Animation_Dialog);
-//				popup.showAtLocation(popupView, Gravity.CENTER, 0, 0);
+//		Button button = (Button) findViewById(R.id.add_room_btn);
+//		button.setOnClickListener( new OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				AlertDialog.Builder builder = new AlertDialog.Builder(getBaseContext());
+//				View dialogView = getLayoutInflater().inflate(R.layout.add_name_dialog, null);
 //				
-//				LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//				builder.setView( dialogView )
+//					.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+//						
+//						@Override
+//						public void onClick(DialogInterface dialog, int which) {
+//							// code to insert new room							
+//						}
+//					})
+//					.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+//						
+//						@Override
+//						public void onClick(DialogInterface dialog, int which) {
+//							dialog.cancel();
+//						}
+//					});
 //				
-//				PopupWindow addNamePopup = new PopupWindow( inflater.inflate(R.layout.add_name_popup, null, false) );
-//				addNamePopup.showAtLocation(parent, Gravity.CENTER, 0, 0);
-				
-			}
-		});
+//				AlertDialog dialog = builder.create();
+//				dialog.show();
+//				
+//				
+////				View popupView = getLayoutInflater().inflate(R.layout.add_name_popup, null);
+////				PopupWindow popup = new PopupWindow(popupView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, true);
+////				popup.setAnimationStyle(android.R.style.Animation_Dialog);
+////				popup.showAtLocation(popupView, Gravity.CENTER, 0, 0);
+////				
+////				LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+////				
+////				PopupWindow addNamePopup = new PopupWindow( inflater.inflate(R.layout.add_name_popup, null, false) );
+////				addNamePopup.showAtLocation(parent, Gravity.CENTER, 0, 0);
+//				
+//			}
+//		});
 	}
 
 	@Override
@@ -72,4 +66,50 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public static class MyDialogFragment extends DialogFragment {
+		int mNum;
+		
+		static MyDialogFragment newInstance( int num ) {
+			MyDialogFragment f = new MyDialogFragment();
+			
+			Bundle args = new Bundle();
+			args.putInt("num", num);
+			f.setArguments(args);
+			
+			return f;
+		}
+		
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			mNum = getArguments().getInt("num");
+			
+			// Pick a style based on the num.
+			int style = DialogFragment.STYLE_NORMAL;
+			int theme = android.R.style.Theme_Holo;
+			switch ((mNum-1)%6) {
+				case 1: style = DialogFragment.STYLE_NO_TITLE; break;
+				case 2: style = DialogFragment.STYLE_NO_FRAME; break;
+				case 3: style = DialogFragment.STYLE_NO_INPUT; break;
+				case 4: style = DialogFragment.STYLE_NORMAL; break;
+			}
+			
+			switch ((mNum-1)%6) {
+				case 4: theme = android.R.style.Theme_Holo; break;
+				case 5: theme = android.R.style.Theme_Holo_Light_Dialog; break;
+				case 6: theme = android.R.style.Theme_Holo_Light; break;
+				case 7: theme = android.R.style.Theme_Holo_Light_Panel; break;
+				case 8: theme = android.R.style.Theme_Holo_Light; break;				
+			}
+
+			setStyle(style, theme);
+		}
+		
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ) {
+			View view = inflater.inflate( R.layout.add_name_dialog, container, false );
+			View textView = findViewByID(R.id.add_room_instruction);
+			
+		}
+	}
 }
