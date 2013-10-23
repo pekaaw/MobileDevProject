f08ef3d773c9aba7e898bb8220f27f8b1e8d6f04
PK <pekaaw@gmail.com>
2013-10-23 17:22:13 +0200

    Added popup to enter name of new room.

--- src/hig/imt3672/mobiledevproject/MainActivity.java
+++ src/hig/imt3672/mobiledevproject/MainActivity.java
@@ -1,8 +1,16 @@
 package hig.imt3672.mobiledevproject;
 
-import android.os.Bundle;
+import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.PopupWindow;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +18,24 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button button = (Button) findViewById(R.id.add_room_btn);
+		button.setOnClickListener( new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				View popupView = getLayoutInflater().inflate(R.layout.add_name_popup, null);
+				PopupWindow popup = new PopupWindow(popupView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, true);
+				popup.setAnimationStyle(android.R.style.Animation_Dialog);
+				popup.showAtLocation(popupView, Gravity.CENTER, 0, 0);
+//				
+//				LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//				
+//				PopupWindow addNamePopup = new PopupWindow( inflater.inflate(R.layout.add_name_popup, null, false) );
+//				addNamePopup.showAtLocation(parent, Gravity.CENTER, 0, 0);
+				
+			}
+		});
 	}
 
 	@Override
