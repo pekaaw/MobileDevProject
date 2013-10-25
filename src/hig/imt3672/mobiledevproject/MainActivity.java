2ee943362ac626bd35d8117713a4ddda421f82ff
PK <pekaaw@gmail.com>
2013-10-25 10:23:29 +0200

    Got a functional dialog. Yay.

--- src/hig/imt3672/mobiledevproject/MainActivity.java
+++ src/hig/imt3672/mobiledevproject/MainActivity.java
@@ -1,13 +1,14 @@
 package hig.imt3672.mobiledevproject;
 
-import android.app.Dialog;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements AddRoomDialog.Communicator{
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +21,9 @@ public class MainActivity extends FragmentActivity {
 			
 			@Override
 			public void onClick(View v) {
-				
+					
 				// custom dialog
-				Dialog dialog = new Dialog(getApplicationContext());
-				dialog.setContentView(R.layout.add_name_dialog);
-				dialog.show();
+				showDialog(v);
 			}
 			
 		});
@@ -37,6 +36,26 @@ public class MainActivity extends FragmentActivity {
 		return true;
 	}
 	
+	public void showDialog(View view) {
+		FragmentManager manager = getFragmentManager();
+		AddRoomDialog addRoomDialog = new AddRoomDialog();
+		addRoomDialog.show(manager, "Add a new room.");
+	}
+	
+//	@Override
+//	public void onDialogMessage(String message) {
+//		// TODO Auto-generated method stub
+//		Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+//	}
+	
+	@Override
+	public void onAddRoomNameRecieved(String name) {
+		// TODO: Integrity check on name
+		// TODO: Add name to new instance, then add celltower and wifi networks.
+		
+		// Toast to debug purposes. To be deleted..
+		Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+	}
 //	public void showDialog() {
 //		AlertDialog alertDialog = new AlertDialog.Builder(getBaseContext()).create();
 //		alertDialog.setTitle("Alert Dialog");
