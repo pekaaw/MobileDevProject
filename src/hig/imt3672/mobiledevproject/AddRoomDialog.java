7853d2efb84cae57a2de3549836229a424529a9d
PK <pekaaw@gmail.com>
2013-10-28 14:38:54 +0100

    Have a functional dialog for adding new room. Title works, but not icons...

--- src/hig/imt3672/mobiledevproject/AddRoomDialog.java
+++ src/hig/imt3672/mobiledevproject/AddRoomDialog.java
@@ -1,12 +1,14 @@
 package hig.imt3672.mobiledevproject;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 
 public class AddRoomDialog extends DialogFragment implements View.OnClickListener {
 	
@@ -16,17 +18,40 @@ public class AddRoomDialog extends DialogFragment implements View.OnClickListene
 	@Override
 	public void onAttach(Activity activity) {
 		// TODO Auto-generated method stub
+		
 		super.onAttach(activity);
 		communicator = (Communicator) activity;
 	}
+	
+//	@Override
+//	public Dialog onCreateDialog(Bundle savedInstanceState) {
+//		LayoutInflater inflater = (LayoutInflater) getActivity().getApplicationContext().getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+//		View view = inflater.inflate(R.layout.add_name_dialog, null);
+//		
+//		view.findViewById(R.id.add_room_ok).setOnClickListener(this);
+//		view.findViewById(R.id.add_room_cancel).setOnClickListener(this);
+//		
+//		Builder dialog = new AlertDialog.Builder(getActivity());
+//		dialog.setIcon(R.drawable.ic_launcher);
+//		dialog.setView(view);
+//		return dialog.create();
+//	};
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		
 		Log.d("onCreate", "Dialog created.");
 		
+		
+		getDialog().getWindow().requestFeature(Window.FEATURE_LEFT_ICON);
+	
+		getDialog().setTitle(R.string.add_room);
+		
+		getDialog().getWindow().setFeatureDrawableResource(Window.FEATURE_LEFT_ICON, R.drawable.add_room);
+		
 		m_view = inflater.inflate(R.layout.add_name_dialog, null);
 		
+	
 		// Make buttons clickable
 		m_view.findViewById(R.id.add_room_ok).setOnClickListener(this);
 		m_view.findViewById(R.id.add_room_cancel).setOnClickListener(this);
