7cbffce079c6f4f196ce50b210743b6a92647db1
PK <pekaaw@gmail.com>
2013-10-30 13:02:42 +0100

    Added a dialog for deleting stuff. Also updated the icons to a standard grey color aaa, and added the add-room-button to the actionbar.

---
+++ src/hig/imt3672/mobiledevproject/DeleteRoomDialog.java
@@ -0,0 +1,58 @@
+package hig.imt3672.mobiledevproject;
+
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+public class DeleteRoomDialog extends DialogFragment implements View.OnClickListener{
+	
+	Communicator communicator;
+	
+	@Override
+	public void onStart() {
+		getDialog().getWindow().setFeatureDrawableResource(Window.FEATURE_LEFT_ICON, R.drawable.delete_room);
+		super.onStart();
+	}
+	
+	@Override
+	public void onAttach(Activity activity) {
+		communicator = (Communicator) activity;
+		super.onAttach(activity);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		
+		getDialog().getWindow().requestFeature(Window.FEATURE_LEFT_ICON);
+		getDialog().setTitle(R.string.delete_room_headline);
+		
+		View m_view = inflater.inflate(R.layout.delete_name_dialog, null);
+		m_view.findViewById(R.id.delete_room_ok).setOnClickListener(this);
+		m_view.findViewById(R.id.delete_room_cancel).setOnClickListener(this);
+		
+		return m_view;
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()) {
+		case R.id.delete_room_cancel :
+			dismiss();
+			communicator.onDeleteCommandReceived(false);	// Do not delete!
+			break;
+		case R.id.delete_room_ok :
+			dismiss();
+			communicator.onDeleteCommandReceived(true);		// Deletion is hereby done!
+			break;
+
+		}
+	}
+	
+	interface Communicator {
+		public void onDeleteCommandReceived(Boolean command);
+	}
+}
--- src/hig/imt3672/mobiledevproject/MainActivity.java
+++ src/hig/imt3672/mobiledevproject/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends FragmentActivity implements AddRoomDialog.Communicator{
+public class MainActivity extends FragmentActivity implements AddRoomDialog.Communicator, DeleteRoomDialog.Communicator{
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -59,11 +59,12 @@ public class MainActivity extends FragmentActivity implements AddRoomDialog.Comm
 		FragmentManager manager = getFragmentManager();
 		AddRoomDialog addRoomDialog = new AddRoomDialog();
 		addRoomDialog.show(manager, "add_room_dialog_id");
-		//addRoomDialog.getDialog().getWindow().setFeatureDrawableResource(Window.FEATURE_LEFT_ICON, R.drawable.add_room);
 	}
 	
 	public void deleteRoomDialog(View view) {
-		//FragmentManager manager = getFragmentManager();
+		FragmentManager manager = getFragmentManager();
+		DeleteRoomDialog deleteRoomDialog = new DeleteRoomDialog();
+		deleteRoomDialog.show(manager, "delete_room_dialog_id");
 		
 	}
 	
@@ -84,6 +85,22 @@ public class MainActivity extends FragmentActivity implements AddRoomDialog.Comm
 		
 		// Toast to debug purposes. To be deleted..
 		Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+	}
+
+	/**
+	 * onDeleteCommandReceived(Boolean command)
+	 * <p/>
+	 * A request to delete a room has been posted.
+	 * <p/>
+	 * Find the room identifier and delete it from the list and from the database.
+	 * 
+	 * @param command True upon deletion, otherwise false.
+	 */
+	@Override
+	public void onDeleteCommandReceived(Boolean command) {
+		if( command == true ) {
+//			delete();	// To delete room. Remember: the identity of the room must be found somewhere...
+		}
 	}
 
 }
