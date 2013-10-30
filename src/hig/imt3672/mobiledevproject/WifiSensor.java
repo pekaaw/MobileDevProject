2bfcd05f7e3505145216f3c0491095ddad2694c6
Sveinung <Trresdal>
2013-10-30 13:31:40 +0100

    WifiSensor has been updated to be an IntentService. It currently fills a list, but we're working on figuring out how to return the data...

--- src/hig/imt3672/mobiledevproject/WifiSensor.java
+++ src/hig/imt3672/mobiledevproject/WifiSensor.java
@@ -9,10 +9,7 @@
  * http://developer.android.com/guide/components/services.html
  * 
  * Thanks, stackoverflow.com
- * 
- * HELP:
- * If you are unfamiliar with what a ScanResult contains, this document is okay:
- * http://developer.android.com/reference/android/net/wifi/ScanResult.html*/
+ */
 
 package hig.imt3672.mobiledevproject;
 
@@ -20,29 +17,45 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import android.app.Service;
+import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
-import android.os.IBinder;
+import android.util.Log;
 
-public class WifiSensor extends Service {
+public class WifiSensor extends IntentService {
+	
 	WifiManager wifi;
 	List<ScanResult> networks;
+	
+	public WifiSensor() {
+		super("WifiSensorService");
+		// TODO Auto-generated constructor stub
+	}
 
-	public List<ScanResult> onStartCommand(int startId) {
+	/*TIP:
+	 * For info on ScanResult:
+	 * http://developer.android.com/reference/android/net/wifi/ScanResult.html
+	 */
+	
+	@Override
+	public void onHandleIntent(Intent intent) {
+		Log.d ("WifiSensor","Starting service.");
+		wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+		
+		Log.d ("WifiSensor","Filling list.");
 		networks = wifi.getScanResults();
+
+		Log.i ("WifiSensor","There are " + Integer.toString(networks.size()) + " networks.");
+		
+		//Sorts by the BSSID of the ScanResults.
 		Collections.sort(networks, new Comparator<ScanResult>() {
 			public int compare(ScanResult s1, ScanResult s2) {
 				return s1.BSSID.compareToIgnoreCase(s2.BSSID);
 			}
 		});
-		return networks;
-	}
-
-	@Override
-	public IBinder onBind(Intent arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		
 	}
 }
